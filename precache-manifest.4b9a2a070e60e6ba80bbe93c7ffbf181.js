912a49f2ca922577d8cd857a518065e3144a3d48
novacane <mudakmudaka@gmail.com>
2020-09-17 21:01:51 -0500

    Updates

---
+++ precache-manifest.4b9a2a070e60e6ba80bbe93c7ffbf181.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553a27ed6fcb2f5a489502bdc709abf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf9f08ac13aba474236a",
+    "url": "/static/css/main.a5b6bd75.chunk.css"
+  },
+  {
+    "revision": "3071573134ca78e60fad",
+    "url": "/static/js/2.26f5b9ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26f5b9ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9f08ac13aba474236a",
+    "url": "/static/js/main.e725836a.chunk.js"
+  },
+  {
+    "revision": "18dbc536a719e4e48975",
+    "url": "/static/js/runtime-main.0032cf3b.js"
+  }
+]);
