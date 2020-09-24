84cc68710ada0d1f96a6e853e4cddf994dbca2e5
novacane <mudakmudaka@gmail.com>
2020-09-23 19:04:41 -0500

    header agregado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import GradCard from "./componentes/GradCard";
+import Nav from "./componentes/Nav";
 import "./App.css";
 
 function App() {
   const lista = [...Array(24)].map((_, index) => <GradCard key={index} />);
 
-  return <div className="App">{lista}</div>;
+  return (
+    <>
+      <Nav />
+      <div className="App">{lista}</div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/componentes/Nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav>
+      <h4>Random Gradient Generator</h4>
+    </nav>
+  );
+}
